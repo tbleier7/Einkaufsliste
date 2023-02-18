791f3cbdf827fd983d5dc83d28cba7dbdc714bae
TobiasBleier <tobias-bleier@web.de>
2023-02-18 15:01:17 +0100

    added a persistenceAdapter for zutaten, maybe rename it later to repository

--- src/main/java/com/tbleier/kitchenlist/application/AddZutatService.java
+++ src/main/java/com/tbleier/kitchenlist/application/AddZutatService.java
@@ -2,12 +2,23 @@ package com.tbleier.kitchenlist.application;
 
 import com.tbleier.kitchenlist.application.ports.in.CommandService;
 import com.tbleier.kitchenlist.application.ports.in.commands.AddZutatCommand;
+import com.tbleier.kitchenlist.application.ports.out.ZutatPersistenceAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AddZutatService implements CommandService<AddZutatCommand> {
+
+    private final ZutatPersistenceAdapter zutatPersistenceAdapter;
+
+    @Autowired
+    public AddZutatService(ZutatPersistenceAdapter zutatPersistenceAdapter) {
+        this.zutatPersistenceAdapter = zutatPersistenceAdapter;
+    }
+
     @Override
     public void execute(AddZutatCommand command) {
         System.out.println(command.getZutat().getName());
+        zutatPersistenceAdapter.save(command.getZutat());
     }
 }
--- src/main/java/com/tbleier/kitchenlist/application/ports/out/ZutatPersistenceAdapter.java
+++ src/main/java/com/tbleier/kitchenlist/application/ports/out/ZutatPersistenceAdapter.java
@@ -1,4 +1,7 @@
 package com.tbleier.kitchenlist.application.ports.out;
 
+import com.tbleier.kitchenlist.application.domain.Zutat;
+
 public interface ZutatPersistenceAdapter {
+    void save(Zutat zutat);
 }

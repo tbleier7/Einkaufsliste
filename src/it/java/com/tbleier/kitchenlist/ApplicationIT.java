bc6eabc27278de5c546019481e3a8341a59eae7d
TobiasBleier <tobias-bleier@web.de>
2023-02-19 11:14:08 +0100

    enabled test for building the application context

--- src/it/java/com/tbleier/kitchenlist/ApplicationIT.java
+++ src/it/java/com/tbleier/kitchenlist/ApplicationIT.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-class ApplicationIT {
+public class ApplicationIT {
 
     @Test
     void should_build_dependency_tree() {

55b24d414a89bc8f01bb1cf930022a75c7fa3ae7
TobiasBleier <tobias-bleier@web.de>
2023-02-19 11:21:36 +0100

    moved definition to java 17 and added spring-boot-starter-data-jpa with a IT test

---
+++ src/it/java/com/tbleier/kitchenlist/adapter/out/ArtikelPersistenceAdapterIT.java
@@ -0,0 +1,15 @@
+package com.tbleier.kitchenlist.adapter.out;
+
+import org.junit.jupiter.api.Test;
+
+public class ArtikelPersistenceAdapterIT {
+    
+    @Test
+    public void should_just_be_discovered() {
+        //Arrange
+    
+        //Act
+    
+        //Assert
+    }
+}
